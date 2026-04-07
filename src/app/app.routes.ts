eb4997c39eae78dd5d43500a0d89335810502571
Rafael Hernandez Pérez <rafaelhernandezperez@Rafaels-MacBook-Air-2.local>
2026-04-07 22:29:39 +0200

    feat: login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { CompanyDetailComponent } from './pages/company-detail/company-detail';
+import { PortfolioComponent } from './pages/portfolio/portfolio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'portfolio/:symbol', component: CompanyDetailComponent },
+];
